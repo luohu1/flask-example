5380dd3d1c5510e9a46a909f3736e844df6d54e1
luohu1 <mylhmail@163.com>
2020-03-06 17:50:58 +0800

    refactor(api): delete api view.

--- app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-
-from .users import UserAPi
-
-app = Blueprint('api', __name__)
-api = Api(app)
-
-api.add_resource(UserAPi, '/users')
--- app/api/users.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-
-
-class UserAPi(Resource):
-    def get(self):
-        return "GET /api/users"
-
-    def post(self):
-        return "POST /api/users"
