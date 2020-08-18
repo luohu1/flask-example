77a07dba25028af6aa59d8bd17cfd354b679cfad
luohu1 <mylhmail@163.com>
2020-08-18 19:13:16 +0800

    refactor: account app.

--- app/account/app.py
+++ app/account/app.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views import UserDetail, UserList
+from .views import Login, Logout, UserDetail, UserList
 
 app = Blueprint('users', __name__, url_prefix='/users')
 api = Api(app)
 
 api.add_resource(UserList, '/')
 api.add_resource(UserDetail, '/<name>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
--- app/account/views.py
+++ app/account/views.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource
 
 from .models import UserModel
@@ -23,3 +24,26 @@ class UserDetail(Resource):
     def get(self, name):
         user = UserModel.query.filter_by(username=name).first_or_404()
         return jsonify(data=user.to_dict())
+
+
+class Login(Resource):
+    def post(self):
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"})
+
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+        if not username or not password:
+            return jsonify({"msg": "Missing username/password parameter"})
+
+        user = UserModel.query.filter_by(username=username).first_or_404()
+        if user:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token)
+
+        return jsonify({"msg": "User not found"})
+
+
+class Logout(Resource):
+    def get(self):
+        return "logout"
