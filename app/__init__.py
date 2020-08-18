f90a0798ffa7cfa4f7c22a6ac9a3b6809c76bd5a
luohu1 <mylhmail@163.com>
2020-08-18 17:55:00 +0800

    feat: register core app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 from flask import Blueprint, Flask
-from werkzeug.utils import find_modules, import_string
+from werkzeug.utils import import_string
 
 from app.common.errors import register_error_handlers
 from app.common.logging import configure_logging
@@ -17,8 +17,9 @@ def create_app(config_name):
 
     configure_logging(app)
     register_extentions(app)
-    register_blueprints(app)
     register_error_handlers(app)
+
+    register_blueprints(app, ['core'])
 
     return app
 
@@ -28,10 +29,16 @@ def register_extentions(app):
     db.init_app(app)
 
 
-def register_blueprints(app):
-    for name in find_modules('app.views'):
-        logger.debug('find modules: %s', name)
-        view = import_string(name)
-        blueprint = getattr(view, 'app', None)
-        if blueprint and isinstance(blueprint, Blueprint):
-            app.register_blueprint(blueprint)
+def register_blueprints(app, blueprints, prefix='app'):
+    for blueprint in blueprints:
+        module_string = '.'.join([prefix, blueprint, 'app'])
+        try:
+            module = import_string(module_string)
+        except Exception:
+            logger.error('module: {} can not import.'.format(module_string))
+            continue
+        blueprint_obj = getattr(module, 'app', None)
+        if blueprint_obj and isinstance(blueprint_obj, Blueprint):
+            app.register_blueprint(blueprint_obj)
+        else:
+            logger.error('can not load blueprint {}'.format(blueprint))
